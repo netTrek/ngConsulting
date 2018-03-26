7a7aac4c30f418acfc28b4172a5f7c4466697f69
Saban Uenlue - netTrek <us@netTrek.de>
2018-03-26 07:29:26 +0200

    Form-Helper

--- src/app/form/model-driven/model-driven.component.ts
+++ src/app/form/model-driven/model-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component ( {
   selector   : 'msg-model-driven',
@@ -8,40 +8,57 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 } )
 export class ModelDrivenComponent implements OnInit {
 
-  get credentialsGroup (): FormGroup {
-    return this.myForm.controls.credentials as FormGroup;
-  }
+  // get credentialsGroup (): FormGroup {
+  //   return this.myForm.controls.credentials as FormGroup;
+  // }
+  //
+  // get emailCtrl (): FormControl {
+  //   if ( this.credentialsGroup ) {
+  //     return this.credentialsGroup.controls.email as FormControl;
+  //   }
+  //   return undefined;
+  // }
+  //
+  // get passwordCtrl (): FormControl {
+  //   if ( this.credentialsGroup ) {
+  //     return this.credentialsGroup.controls.password as FormControl;
+  //   }
+  //   return undefined;
+  // }
 
-  get emailCtrl (): FormControl {
-    if ( this.credentialsGroup ) {
-      return this.credentialsGroup.controls.email as FormControl;
-    }
-    return undefined;
-  }
+  myForm: FormGroup;
+  nameCtrl: FormControl;
+  credentialsGroup: FormGroup;
+  emailCtrl: FormControl;
+  passwordCtrl: FormControl;
 
-  get passwordCtrl (): FormControl {
-    if ( this.credentialsGroup ) {
-      return this.credentialsGroup.controls.password as FormControl;
-    }
-    return undefined;
-  }
-
-  public myForm: FormGroup;
-
-  constructor () {
+  constructor ( private fb: FormBuilder ) {
   }
 
   ngOnInit () {
 
-    this.myForm = new FormGroup ( {
-      name       : new FormControl ( 'Saban', Validators.required ),
-      credentials: new FormGroup ( {
-        email   : new FormControl ( 'us@netTrek.de', [ Validators.email,
-                                                       Validators.required
-        ] ),
-        password: new FormControl ( 'test1234', Validators.required )
-      } )
-    } );
+    this.myForm = this.fb.group( {
+      name: [ 'Saban', Validators.required ],
+      credentials: this.fb.group ( {
+        email: ['us@netTrek.de', [ Validators.email, Validators.required]],
+        password: ['test1234', Validators.required ]
+      })
+    });
+
+    this.nameCtrl = this.myForm.get( 'name') as FormControl;
+    this.credentialsGroup = this.myForm.get( 'credentials') as FormGroup;
+    this.emailCtrl = this.myForm.get( ['credentials', 'email'] ) as FormControl;
+    this.passwordCtrl = this.myForm.get( ['credentials', 'password']) as FormControl;
+
+    // this.myForm = new FormGroup ( {
+    //   name       : new FormControl ( 'Saban', Validators.required ),
+    //   credentials: new FormGroup ( {
+    //     email   : new FormControl ( 'us@netTrek.de', [ Validators.email,
+    //                                                    Validators.required
+    //     ] ),
+    //     password: new FormControl ( 'test1234', Validators.required )
+    //   } )
+    // } );
   }
 
   send () {
